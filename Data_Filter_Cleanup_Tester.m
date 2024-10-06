9442ee1712f9b0d0bdb2136c8b1b42f91ee47453
Nelson <mrnelsonlee@outlook.com>
2024-10-06 16:22:03 -0700

    Minor changes

--- Data_Filter_Cleanup_Tester.m
+++ Data_Filter_Cleanup_Tester.m
@@ -7,7 +7,7 @@ for i = 1:length(subFolders)
     fprintf('Processing subfolder: %s\n', currentSubFolder);
     
     csvFiles = dir(fullfile(currentSubFolder, '*.csv'));
-    fprintf('Found %d CSV files in subfolder\n', length(csvFiles));
+    fprintf('Foud %d CSV files in subfolder\n', length(csvFiles));
     
     if isempty(csvFiles)
         fprintf('No CSV files found in subfolder!\n');
